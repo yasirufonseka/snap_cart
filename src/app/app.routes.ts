01c5b990c72b25bbbc51235f90a3796a28786a94
yasirufonseka <150530375+yasirufonseka@users.noreply.github.com>
2025-09-22 17:43:50 +0530

    initial commit
    initial commit with navbar and carousal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CarousalComponent } from './components/carousal/carousal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "navbar", component: NavBarComponent },
+    { path: "carousal", component: CarousalComponent }
+];
---
+++ src/app/components/carousal/carousal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousal',
+  imports: [],
+  templateUrl: './carousal.component.html',
+  styleUrl: './carousal.component.scss'
+})
+export class CarousalComponent {
+
+}
