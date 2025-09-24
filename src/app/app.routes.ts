7b7139c36ec55ec193b8af2ccad3d9b2be58ee3b
yasirufonseka <150530375+yasirufonseka@users.noreply.github.com>
2025-09-24 07:30:49 +0530

    adding ad-card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { CarousalComponent } from './components/carousal/carousal.component';
+import { AdCardComponent } from './components/ad-card/ad-card.component';
 
 export const routes: Routes = [
     { path: "navbar", component: NavBarComponent },
---
+++ src/app/components/ad-card/ad-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-card',
+  imports: [],
+  templateUrl: './ad-card.component.html',
+  styleUrl: './ad-card.component.scss'
+})
+export class AdCardComponent {
+
+}
